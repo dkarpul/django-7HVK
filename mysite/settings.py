2806c1c24fb57fa87369dd8e8d6f42463219ccfa
dkarpul <dkarpul@gmail.com>
2023-04-10 19:33:34 +0200

    new file:   Django_railway_2.code-workspace 	modified:   mysite/__pycache__/settings.cpython-39.pyc 	modified:   mysite/__pycache__/urls.cpython-39.pyc 	modified:   mysite/settings.py 	modified:   mysite/urls.py 	modified:   requirements.txt 	new file:   streamlit/__init__.py 	new file:   streamlit/__pycache__/__init__.cpython-39.pyc 	new file:   streamlit/__pycache__/admin.cpython-39.pyc 	new file:   streamlit/__pycache__/apps.cpython-39.pyc 	new file:   streamlit/__pycache__/models.cpython-39.pyc 	new file:   streamlit/__pycache__/urls.cpython-39.pyc 	new file:   streamlit/__pycache__/views.cpython-39.pyc 	new file:   streamlit/admin.py 	new file:   streamlit/apps.py 	new file:   streamlit/migrations/__init__.py 	new file:   streamlit/migrations/__pycache__/__init__.cpython-39.pyc 	new file:   streamlit/models.py 	new file:   streamlit/templates/streamlit/home.html 	new file:   streamlit/tests.py 	new file:   streamlit/urls.py 	new file:   streamlit/views.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -35,6 +35,7 @@ CSRF_TRUSTED_ORIGINS = ["https://django-server-production-a430.up.railway.app/",
 # Application definition
 
 INSTALLED_APPS = [
+    'streamlit.apps.StreamlitConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -78,23 +79,23 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ["PGDATABASE"],
-        'USER': os.environ["PGUSER"],
-        'PASSWORD': os.environ["PGPASSWORD"],
-        'HOST': os.environ["PGHOST"],
-        'PORT': os.environ["PGPORT"],
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': os.environ["PGDATABASE"],
+#         'USER': os.environ["PGUSER"],
+#         'PASSWORD': os.environ["PGPASSWORD"],
+#         'HOST': os.environ["PGHOST"],
+#         'PORT': os.environ["PGPORT"],
+#     }
+# }
 
 
 # Password validation
