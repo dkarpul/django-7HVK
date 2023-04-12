a49f99ea48d9528d3ff8327b606c4c67944642a8
dkarpul <dkarpul@gmail.com>
2023-04-12 23:02:15 +0200

    modified:   railway.json 	modified:   stapp.py

--- stapp.py
+++ stapp.py
@@ -7,3 +7,5 @@ def main():
 if __name__ == "__main__":
     app = main()  # Create the Streamlit app instance
     app.run(debug=True)  # Run the app with debugging enabled
+    
+    & gunicorn --bind 0.0.0.0:8501 \"streamlit run app.py\""
