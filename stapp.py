903b77601132a924c575f4719fbbe52d5ad3b679
dkarpul <dkarpul@gmail.com>
2023-04-12 23:11:32 +0200

    modified:   stapp.py

--- stapp.py
+++ stapp.py
@@ -2,10 +2,10 @@ import streamlit as st
 
 def main():
     st.title("My Streamlit App")
-    st.write("Hello, world!")
+    st.write("Hello, worldd!")
 
 if __name__ == "__main__":
     app = main()  # Create the Streamlit app instance
     app.run(debug=True)  # Run the app with debugging enabled
     
-    & gunicorn --bind 0.0.0.0:8501 \"streamlit run app.py\""
+    & gunicorn -- bind 0.0.0.0:8501 \"streamlit run app.py\""
